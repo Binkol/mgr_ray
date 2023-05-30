fac5900a342b12b33e8fb1bccbf6f00bd53ad187
Tomasz Michalik <t.michalik630@gmail.com>
2023-05-31 01:01:41 +0200

    added placement groups

--- my_ray.py
+++ my_ray.py
@@ -2,42 +2,55 @@ import ray
 import time
 from cbcf import UserBasedCF
 import pandas as pd
-
-@ray.remote
-def calc(ubcf, userid):
-    reco = ubcf.generateRecomendations.remote(userid)
-    return reco
+from ray.util.placement_group import (
+    placement_group,
+    placement_group_table,
+    remove_placement_group,
+)
+from ray.util.scheduling_strategies import PlacementGroupSchedulingStrategy
 
 if __name__ == "__main__":
-    ray.init(_node_ip_address='192.168.1.163')
+    ray.init(_node_ip_address='192.168.100.146')
 
     ratings = pd.read_csv('data/ml-latest-small/ratings.csv')
     movies = pd.read_csv('data/ml-latest-small/movies.csv')
 
     start_time = time.time()
 
-    cluster_size = 2
-    actors = [UserBasedCF.remote(ratings,
-                                movies,
-                                numberOfSimilarUsers=10,
-                                similarityThreshold=0.3) for _ in range(cluster_size)]
-    
 
-    #futures = [calc.remote(UBCF, i) for i in range(1,10)]
-    futures = []
-    for id in range(1,100):
-        cluster_id = id%cluster_size
-        ref = calc.remote(actors[cluster_id], id)
-        futures.append(ref)
-        #time.sleep(0.5)
-
-    output = ray.get(futures)
+    cluster_size = 4
+    pg_list = [{"CPU": 4} for _ in range(cluster_size)]
+    pg = placement_group(pg_list, strategy="SPREAD")
     
-    counter = 1
-    for x in output:
-        print("=== {} recomendation ===".format(counter))      
-        print(ray.get(x).head(5))
-        counter += 1
-  
-    #print(ray.get(output[88]).head(5))
-    print(time.time() - start_time)
+    try:
+        ray.get(pg.ready(), timeout=10)
+
+        actors = [UserBasedCF.remote(ratings,
+                                    movies,
+                                    numberOfSimilarUsers=10,
+                                    similarityThreshold=0.3) for _ in range(cluster_size)]
+        
+
+        futures = []
+        for id in range(1,300):
+            cluster_id = id%cluster_size
+            ref = actors[cluster_id].generateRecomendations.remote(id)
+            futures.append(ref)
+            #time.sleep(0.5)
+
+        #output = ray.get(futures)
+        
+        counter = 1
+        for x in ray.get(futures):
+            print("=== {} recomendation ===".format(counter))      
+            print(x.head(5))
+            counter += 1
+    
+        #print(ray.get(output[88]).head(5))
+        print(time.time() - start_time)
+
+    except Exception as e:
+        print(
+            "Cannot create a placement group because "
+        )
+        print(e)
