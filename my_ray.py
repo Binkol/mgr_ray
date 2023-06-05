60d84453df636eae5520c3111415b9fcf52520b3
tmichali <tmichali@localhost.localdomain>
2023-06-05 20:12:19 +0200

    added code for dataset for each actor

--- my_ray.py
+++ my_ray.py
@@ -10,47 +10,46 @@ from ray.util.placement_group import (
 from ray.util.scheduling_strategies import PlacementGroupSchedulingStrategy
 
 if __name__ == "__main__":
-    ray.init(_node_ip_address='192.168.100.146')
-
-    ratings = pd.read_csv('data/ml-latest-small/ratings.csv')
-    movies = pd.read_csv('data/ml-latest-small/movies.csv')
-
-    start_time = time.time()
-
-
+    ray.init(_node_ip_address='192.168.1.70')
     cluster_size = 4
+
+    ratings = []
+    movies = []
+    for data_id in range(1, cluster_size+1):
+        ratings.append(pd.read_csv('data{}/ml-latest-small/ratings.csv'.format(data_id)))
+        movies.append(pd.read_csv('data{}/ml-latest-small/movies.csv'.format(data_id)))
+
     pg_list = [{"CPU": 4} for _ in range(cluster_size)]
     pg = placement_group(pg_list, strategy="SPREAD")
     
+    start_time = time.time()
     try:
         ray.get(pg.ready(), timeout=10)
 
-        actors = [UserBasedCF.remote(ratings,
-                                    movies,
+        actors = [UserBasedCF.remote(ratings[id],
+                                    movies[id],
                                     numberOfSimilarUsers=10,
-                                    similarityThreshold=0.3) for _ in range(cluster_size)]
+                                    similarityThreshold=0.3) for id in range(cluster_size)]
         
 
         futures = []
-        for id in range(1,300):
+        for id in range(1,100):
             cluster_id = id%cluster_size
             ref = actors[cluster_id].generateRecomendations.remote(id)
             futures.append(ref)
-            #time.sleep(0.5)
+            time.sleep(0.5)
 
         #output = ray.get(futures)
         
         counter = 1
         for x in ray.get(futures):
             print("=== {} recomendation ===".format(counter))      
-            print(x.head(5))
+            #print(x.head(5))
             counter += 1
     
         #print(ray.get(output[88]).head(5))
         print(time.time() - start_time)
 
     except Exception as e:
-        print(
-            "Cannot create a placement group because "
-        )
+        print("Cannot create a placement group because ")
         print(e)
