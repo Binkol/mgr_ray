f45fa2694a74207bb43d33cf97e7c302b127a0b9
Tomasz Michalik <t.michalik630@gmail.com>
2023-08-06 13:34:40 +0200

    CBR half movies

--- cbr.py
+++ cbr.py
@@ -38,6 +38,7 @@ class CBR:
         return tf_idf_matrix
 
     def index_from_title(self, df, title):
+        #print(df[df['original_title']==title].index.values[0], "BBBBBBB")
         return df[df['original_title']==title].index.values[0]
 
     # function that returns the title of the movie from its index
@@ -51,7 +52,7 @@ class CBR:
 
     # generating recommendations for given title
     def recommendations(self, original_title, number_of_recommendations):
-        index = self.index_from_title(self.df,original_title)
+        index = self.index_from_title(movies,original_title)
         similarity_scores = list(enumerate(self.cosine_similarity_matrix[index]))
         similarity_scores_sorted = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
         recommendations_indices = [t[0] for t in similarity_scores_sorted[1:(number_of_recommendations+1)]]
@@ -59,8 +60,22 @@ class CBR:
     
 if __name__ == "__main__":
     movies = pd.read_csv('data/kaggle/movies_metadata.csv', low_memory=False)
+    
+    half_rows = len(movies) // 2
+    half_movies = movies.iloc[:half_rows].copy()
+
+    quater_rows = len(movies) // 4
+    quater_movies = movies.iloc[:quater_rows].copy()
+
     titles = movies.original_title.values.tolist()
-    print(titles[0:10])
-    cbr = CBR(movies)
-    rec = cbr.recommendations("True Lies", 5)
-    print(rec)
+    print(quater_movies.info())
+    
+    cbr = CBR(half_movies)
+    for title in titles[0:1000]:
+        print("AAAAAA",title)
+        rec = cbr.recommendations(title, 5)
+        print(rec)
+    
+    # rec = cbr.recommendations("Waiting to Exhale", 5)
+    # #print(rec)
+    # print(cbr.index_from_title(movies, "Waiting to Exhale"))
--- my_ray.py
+++ my_ray.py
@@ -1,6 +1,6 @@
 import ray
 import time
-from cbcf import UserBasedCF
+from ubcf import UserBasedCF
 from ibcf import ItemBasedCF
 from cbr import CBR
 import pandas as pd
@@ -12,13 +12,13 @@ from ray.util.placement_group import (
 from ray.util.scheduling_strategies import PlacementGroupSchedulingStrategy
 
 if __name__ == "__main__":
-    ray.init(_node_ip_address='192.168.1.70')
+    ray.init(_node_ip_address='139.144.77.13')
 
     # ratings = pd.read_csv('data/ml-latest-small/ratings.csv')
     # movies = pd.read_csv('data/ml-latest-small/movies.csv')
 
-    ratings = pd.read_csv('data/kaggle/ratings_small.csv')
-    kaggle_movies = pd.read_csv('data/kaggle/movies_metadata.csv', low_memory=False)
+    ratings = pd.read_csv('~/data/kaggle/ratings_small.csv')
+    kaggle_movies = pd.read_csv('~/data/kaggle/movies_metadata.csv', low_memory=False)
     movies = kaggle_movies[['id','title']]
     movies = movies.rename(columns = {'id':'movieId'})
     movies = movies[(movies.movieId != "1997-08-20") & 
@@ -34,56 +34,63 @@ if __name__ == "__main__":
     ratings300 = ratings[ratings['userId'] <= 300]
     ratings600 = ratings[ratings['userId'] <= 600]
 
-    titles = movies.original_title.values.tolist()
+    half_rows = len(movies) // 2
+    quater_rows = len(movies) // 4
+    
+    half_movies = movies.iloc[:half_rows].copy()
+    quater_movies = movies.iloc[:quater_rows].copy()
+
+    titles = movies.title.values.tolist()
     
     start_time = time.time()
 
-    cluster_size = 4
+    cluster_size = 2
     actors_size = cluster_size*4
     pg_list = [{"CPU": 4} for _ in range(cluster_size)]
     pg = placement_group(pg_list, strategy="SPREAD")
     
     try:
         ray.get(pg.ready(), timeout=10)
-
-        # actors = [UserBasedCF.remote(ratings, # <--
-        #                             movies,
-        #                             numberOfSimilarUsers=10,
-        #                             similarityThreshold=0.3) for _ in range(actors_size)]
-        
-        actors = [ItemBasedCF.remote(ratings, 
-                                    movies, # <--
-                                    number_of_similar_items=5,
-                                    number_of_recommendations=3) for _ in range(actors_size)]
-
-        #actors = [CBR.remote(movies) for _ in range(actors_size)]
-
-
-        run_cbr = False
-
-        futures = []
-        if run_cbr:
-            for title in titles[0:300]:
-                actor_id = id%actors_size
-                ref = actors[actor_id].recommendations.remote(title, 5)
-                futures.append(ref)
-        else:
-            for id in range(1,300):
-                actor_id = id%actors_size
-                ref = actors[actor_id].generateRecomendations.remote(id)
-                futures.append(ref)
-
-        #output = ray.get(futures)
-        
-        counter = 1
-        for x in ray.get(futures):
-            print("=== {} recomendation ===".format(counter))      
-            #print(x.head(5))
-            counter += 1
-    
-        #print(ray.get(output[88]).head(5))
-        print(time.time() - start_time)
-
     except Exception as e:
         print("Cannot create a placement group because ")
         print(e)
+
+    # actors = [UserBasedCF.remote(ratings, # <--
+    #                             movies,
+    #                             numberOfSimilarUsers=10,
+    #                             similarityThreshold=0.3) for _ in range(actors_size)]
+    
+    # actors = [ItemBasedCF.remote(ratings, 
+    #                            movies, # <--
+    #                            number_of_similar_items=5,
+    #                            number_of_recommendations=3) for _ in range(actors_size)]
+
+    actors = [CBR.remote(kaggle_movies) for _ in range(actors_size)]
+
+
+    run_cbr = True
+
+    futures = []
+    if run_cbr:
+        for i, title in enumerate(titles[0:300]):
+            actor_id = (i+1)%actors_size
+            ref = actors[actor_id].recommendations.remote(title, 5)
+            futures.append(ref)
+    else:
+        for id in range(1,300):
+            actor_id = id%actors_size
+            ref = actors[actor_id].generateRecomendations.remote(id)
+            futures.append(ref)
+
+    #output = ray.get(futures)
+    
+    counter = 1
+    for x in ray.get(futures):
+        print("=== {} recomendation ===".format(counter))      
+        #print(x.head(5))
+        counter += 1
+
+    #print(ray.get(output[88]).head(5))
+    print(time.time() - start_time)
+
+    
